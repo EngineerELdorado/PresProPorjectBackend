98e5ea90f504ece61b77d9ff5de5b568f8012d91
Denis Kalenga <engineereldorado@gmail.com>
2019-11-22 12:28:46 -0800

    Initial Commit

---
+++ src/main/java/com/example/PresProProject/PresProProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.PresProProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresProProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresProProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PresProProject/PresProProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PresProProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresProProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
