b19d01f6dede9f118d3e894c28a71985e2495630
Denis Kalenga <engineereldorado@gmail.com>
2019-12-10 11:29:45 -0800

    second commit

--- src/main/java/com/example/PresProProject/controllers/AdminController.java
+++ src/main/java/com/example/PresProProject/controllers/AdminController.java
@@ -11,9 +11,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.*;
-
-import javax.persistence.Entity;
-
 @RestController
 @RequestMapping("/admins")
 public class AdminController {
